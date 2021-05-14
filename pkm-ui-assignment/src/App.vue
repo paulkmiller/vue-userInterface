e04a3ccfb4835c5bdb49b64b62b2f8554d54fa79
Paul Miller <pmillerk88@gmail.com>
2021-05-13 21:27:19 -0400

    First steps

--- pkm-ui-assignment/src/App.vue
+++ pkm-ui-assignment/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App Custom"/> -->
+    <UserIndex />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import UserIndex from './components/UserIndex.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserIndex
   }
 }
 </script>
---
+++ pkm-ui-assignment/src/components/UserIndex.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4>Users Page</h4>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+}
+</script>
