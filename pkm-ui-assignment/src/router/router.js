995df51b2d4112c550eb7eff2390c2ee247eff30
Paul Miller <pmillerk88@gmail.com>
2021-05-14 13:47:52 -0400

    Initial Vuetify install seems good except for one sass-loader plugin failing during compilation. SO threads suggest it is related to incompatability between specific versions of sass-loader and vue.

--- pkm-ui-assignment/src/router/router.js
+++ pkm-ui-assignment/src/router/router.js
@@ -10,17 +10,17 @@ export default new Router({
       path: "/",
       alias: "/users",
       name: "users",
-      component: () => import("./components/UserIndex")
+      component: () => import("../components/UserIndex")
     },
     {
       path: "/users/:id",
       name: "user-details",
-      component: () => import("./components/UserShow")
+      component: () => import("../components/UserShow")
     },
-    // {
-    //   path: "/add",
-    //   name: "add",
-    //   component: () => import("./components/UserAdd")
-    // }
+    {
+      path: "/test",
+      name: "add",
+      component: () => import("../components/UserAdd")
+    }
   ]
 });
