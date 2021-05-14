9531be1429ae6ea301b8f0358833348cb11fefc0
paulkmiller <pmillerk88@gmail.com>
2021-05-14 05:24:15 -0400

    Added vue router and basic router file

---
+++ pkm-ui-assignment/src/router.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      alias: "/users",
+      name: "users",
+      component: () => import("./components/UserIndex")
+    },
+    {
+      path: "/users/:id",
+      name: "user-details",
+      component: () => import("./components/UserShow")
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: () => import("./components/UserAdd")
+    }
+  ]
+});
